b6e146d0563216fda88f0aadcc100ecedcd6fa4b
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-04 22:24:27 +0500

    Add src\reducers\productsReducer.js

---
+++ src\reducers\productsReducer.js
@@ -0,0 +1,14 @@
+import { FETCH_PRODUCTS } from '../actions/types';
+
+const initialState = {
+  products: []
+};
+
+export default function productsReducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_PRODUCTS:
+      return { ...state, products: action.payload };
+    default:
+      return state;
+  }
+}
